e33ab42ebb96273a446e9d207eecd0df8413c053
kremlsa <55805583+kremlsa@users.noreply.github.com>
2026-04-02 08:11:31 +0700

    Create task.java

---
+++ day2/task.java
@@ -0,0 +1,21 @@
+// Дано:
+
+// DTO:
+//
+// class UserDTO {
+//     String email;
+//     String password;
+// }
+
+// Сделать:
+
+// email → валидный формат
+// password:
+// минимум 8 символов
+// 1 цифра
+// 1 спецсимвол
+
+// Использовать:
+
+// @Valid
+// @Pattern
